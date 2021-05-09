3c0e19f480f89e2408c8554f18f0526b21eeb49d
Adam Burstyn <adam.burstyn@gmail.com>
2021-05-09 12:09:41 -0400

    added data table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Select table body element from html
+var tbody = d3.select("tbody");
+
+// Iterate over all entries in the data and append html table
+tableData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
